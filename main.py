3dd30d6c880ccea811753a2664ddaedfb0f3c54e
Merritt Duncan <merrittduncan@gmail.com>
2017-01-22 18:49:48 -0600

    final commits

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
 import webapp2
+import random
+
+def getRandomFortune():
+    fortunes = [
+    'You will live the longest of all!',
+    'Be wary of crustaceans!',
+    'Many orchard fruits will come your way!'
+    ]
+    index = random.randint(0,((len(fortunes))-1))
+
+    return fortunes[index]
+
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello worl!')
+        header = '<h1>Fortune Cookie</h1>'
+        fortune = getRandomFortune()
+        fortune_sentence = "Your fortune: " + str(fortune)
+        fortune_paragraph = '<p>' + fortune_sentence + '</p>'
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        lucky_number = random.randint(1,100)
+        number_sentence = 'Your lucky number is ' + str(lucky_number) + '!'
+        number_paragraph = '<P>' + number_sentence + '</p>'
+
+        more = '<button><a href =".">More!!</a></button>'
+
+        content = header + fortune_paragraph + number_paragraph + more
+
+        self.response.write(content)
+
+routes = [
+    ('/', MainHandler),
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
